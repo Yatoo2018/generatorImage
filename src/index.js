4a84e621cc03042c6439036b5339d37f1de1a42e
Yatoo2018 <tanpf2018@163.com>
2018-08-20 23:39:01 +0800

    add fileupload via createObjectURL

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import * as G from './image-generator.js'
 
 window.onload = function() {
 
-  let g = new G.ImageGenerator({topBannerHeight: 100, bottomBannerHeight: 300, wrap: 'body'})
+  let g = new G.ImageGenerator({topBannerHeight: 100, bottomBannerHeight: 100, wrap: 'body'})
   let stageDom = document.querySelector('#stage')
   let stageHeight = stageDom.getBoundingClientRect().height
   let stageWidth = stageDom.getBoundingClientRect().width
